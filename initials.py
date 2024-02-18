137196a3dd8c1cc6a0930a80956336b6d6454553
Fahikader <151936836+Fahikader@users.noreply.github.com>
2024-02-18 14:59:46 +0200

    displays the initials of name in block letter

---
+++ initials.py
@@ -0,0 +1,11 @@
+#FAHIMA ABDUL KADER
+#I love to do baking, listen to music, and engage in artwork as my hobbies
+#Printing Initials
+
+print("FFFFF    A      K    K")
+print("F      A   A    K   K")
+print("F     A     A   K  K")
+print("FFF  A A A A A  K K")
+print("F    A       A  K  K")
+print("F    A       A  K   K")
+print("F    A       A  K    K")
